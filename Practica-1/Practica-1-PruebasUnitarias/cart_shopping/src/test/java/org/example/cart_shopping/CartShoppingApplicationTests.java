b2322c4cfb71e89dcd29390b9e3903dad27f1415
friasluna01 <sfrias@clickping.do>
2025-05-29 00:34:47 -0400

    initial commit

---
+++ Practica-1/Practica-1-PruebasUnitarias/cart_shopping/src/test/java/org/example/cart_shopping/CartShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cart_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
